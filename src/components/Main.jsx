c30d0add9b562427ea185c20ee2a48df001d583b
YanaWhitte <shiro.kirio@gmail.com>
2021-07-20 20:31:38 +0300

    Buttons hover

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,32 +23,22 @@ const Description = () => {
   </div>
 }
 
-class BlackSquare extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      emailDisplay: "none",
-      phoneDisplay: "none",
-    }
-  }
-
-  render() {
-    return <div className={s.square}>
-      <nav>
-        <ul className={s.menu}>
-          <li><a className={s.menuItem} href="https://github.com/YanaWhitte" target="_blank">github</a></li>
-          <li><a className={s.menuItem} href="https://www.linkedin.com/in/whiony" target="_blank">linkedin</a></li>
-          <li><a className={`${s.menuItem}`} href="mailto:whiony@gmail.com">email</a></li>
-          <li><a className={`${s.menuItem}`} href="tel:+380500877535">call me</a></li>
-          <li><a className={`${s.menuItem} ${s.resume}`} href="/CV.pdf" download>resume</a></li>
-        </ul>
-      </nav>
-      <div className={s.titleBlock}>
-        <h1>Yana Bychilova</h1>
-        <h2>frontend developer</h2>
-      </div>
+const BlackSquare = () => {
+  return <div className={s.square}>
+    <nav>
+      <ul className={s.menu}>
+        <li><a className={s.menuItem} href="https://github.com/YanaWhitte" target="_blank">github</a></li>
+        <li><a className={s.menuItem} href="https://www.linkedin.com/in/whiony" target="_blank">linkedin</a></li>
+        <li><a className={`${s.menuItem}`} href="mailto:whiony@gmail.com">email</a></li>
+        <li><a className={`${s.menuItem}`} href="tel:+380500877535">call me</a></li>
+        <li><a className={`${s.menuItem} ${s.resume}`} href="/CV.pdf" download>resume</a></li>
+      </ul>
+    </nav>
+    <div className={s.titleBlock}>
+      <h1>Yana Bychilova</h1>
+      <h2>frontend developer</h2>
     </div>
-  }
+  </div>
 }
 
 export default Main;
